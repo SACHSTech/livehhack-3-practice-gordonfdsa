00dbd47e7f1941fcd28ad9efdb5c8226ee06ce8a
Gordon <gordon.zhu25@ycdsbk>
2023-12-04 10:19:01 -0500

    Final Code

--- String_2.java
+++ String_2.java
@@ -5,6 +5,7 @@ public class String_2 {
 
   public static void main(String[] args) {
 
+    System.out.println(prefixAgain("abXYabc", 2));
   }
 
   public static String doubleChar(String str) {
@@ -25,13 +26,60 @@ public class String_2 {
     return ans;
   }
 
-  public boolean bobThere(String str) {
+  public static boolean bobThere(String str) {
     for (int i = 0; i < str.length() - 2; i++) {
       if (str.charAt(i) == 'b' && str.charAt(i + 2) == 'b')
         return true;
     }
     return false;
+  }
 
+  public static String repeatEnd(String str, int n) {
+    String ans = "";
+    for (int i = 0; i < n; i++) {
+      ans += str.substring(str.length() - n, str.length());
+    }
+    return ans;
+  }
+
+  public static boolean prefixAgain(String str, int n) {
+    String prefix = str.substring(0, n);
+    str = str.substring(n, str.length());
+    return str.contains(prefix);
+  }
+
+  public static boolean sameStarChar(String str) {
+    // ignore first and last character as it doens't matter anyways if its a star
+    for (int i = 1; i < str.length() - 1; i++) {
+      if (str.charAt(i) == '*') {
+        //if the 2 characters beside the star are not same, return false
+        if (str.charAt(i - 1) != str.charAt(i + 1)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public static String starOut(String str) {
+    boolean[] arr = new boolean[str.length()];
+    String ans = "";
+    for (int i = 0; i < str.length(); i++) {
+      // just to make sure arr doens't go out of bounds
+      if (str.charAt(i) == '*') {
+        arr[Math.min(i + 1, str.length() - 1)] = true;
+        arr[Math.max(i - 1, 0)] = true;
+        arr[i] = true;
+      }
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+      // just to make sure arr doens't go out of bounds
+      if (!arr[i]) {
+        ans += str.charAt(i);
+      }
+    }
+    return ans;
   }
 
 }
